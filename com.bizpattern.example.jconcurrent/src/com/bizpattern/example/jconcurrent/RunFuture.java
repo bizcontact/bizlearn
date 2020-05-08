2fcbc13cabd9e5946b354d1822b124a628270960
Cookie <dinesh1>
2020-05-08 16:10:10 -0400

    Added example for Blocking queue. Producer keep producing values and the generated values are picked up by one of the consumer..

---
+++ com.bizpattern.example.jconcurrent/src/com/bizpattern/example/jconcurrent/RunFuture.java
@@ -0,0 +1,13 @@
+package com.bizpattern.example.jconcurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class RunFuture {
+	
+	public static void main(String[] args) {
+
+		
+	}
+
+}
